64aabf98486d852c391d732eaa1c56b43b9d6129
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2022-11-21 16:50:38 -0500

    carousel functional

---
+++ public/carousel.js
@@ -0,0 +1,30 @@
+// const express = require('express');
+
+$(() => {
+
+  let numberOfImages = $('.carouselImages').children().length - 1;
+  let currentImg = 0;
+
+  $('#previousbtn').on('click', () => {
+        $('.carouselImages').children().eq(currentImg).css('display', 'none');
+        if (currentImg > 0) {
+          currentImg--
+        } else {
+          currentImg = numberOfImages;
+        }
+       $('.carouselImages').children().eq(currentImg).css('display', 'block');
+    })
+
+    $('#nextbtn').on('click', () => {
+        $('.carouselImages').children().eq(currentImg).css('display', 'none');
+        if (currentImg < numberOfImages) {
+          currentImg++
+        } else {
+          currentImg = 0;
+        }
+       $('.carouselImages').children().eq(currentImg).css('display', 'block');
+    })
+
+
+
+});
